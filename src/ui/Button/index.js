1da788dfd30446348dab302c4a127b7bf42dc802
Alexey Taktarov <molefrog@gmail.com>
2017-09-16 22:54:40 +0300

    Support for `loading` prop in Button component.

--- src/ui/Button/index.js
+++ src/ui/Button/index.js
@@ -1,7 +1,33 @@
-import styled from 'styled-components'
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
 import { colors } from '../../styles'
 
-const Button = styled.button`
+import loader from './loading-circle.svg'
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Loader = styled.div`
+  width: 20px;
+  height: 20px;
+  background-image: url(${loader});
+  background-size: contain;
+
+  margin-right: 10px;
+
+  transform-origin: center center;
+  animation: ${rotate360} 0.5s infinite;
+  animation-timing-function: linear;
+`
+
+const ButtonWrapper = styled.button`
   border: 2px solid ${colors.ultraBlue};
   border-radius: 3px;
 
@@ -12,6 +38,22 @@ const Button = styled.button`
 
   font-weight: 600;
   cursor: pointer;
+
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
+  ${Loader} {
+    display: ${props => (props.loading ? 'inline-block' : 'none')};
+  }
+}
 `
+
+const Button = ({ children, loading }) => (
+  <ButtonWrapper loading={loading}>
+    <Loader />
+    <span>{children}</span>
+  </ButtonWrapper>
+)
 
 export default Button
